ded8b014ab37a656011e31cb7b8eb4d5b224043a
nixzhu <zhuhongxu@gmail.com>
2014-11-24 11:31:03 +0800

    init

---
+++ CenterTwoViewsUseAutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CenterTwoViewsUseAutoLayout
+//
+//  Created by NIX on 14/11/21.
+//  Copyright (c) 2014年 nixWork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CenterTwoViewsUseAutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CenterTwoViewsUseAutoLayout
+//
+//  Created by NIX on 14/11/21.
+//  Copyright (c) 2014年 nixWork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
